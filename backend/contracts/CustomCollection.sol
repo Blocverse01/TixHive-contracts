0390bfc1abde90972a3b8cc93a9cf0f7f5e6ce29
Avoaja Joshua Akachi <akachijc@gmail.com>
2022-02-22 21:05:11 +0000

    build updates --3 day speedrun

--- backend/contracts/CustomCollection.sol
+++ backend/contracts/CustomCollection.sol
@@ -6,7 +6,35 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract CustomNFTContract is ERC721URIStorage, Ownable {
-    function mint() {
+    address public _owner;
+    uint256 COUNTER;
+
+    struct Ticket {
+        uint256 id;
+        uint price;
+    }
+    
+    Ticket[] public tickets;
+
+    constructor(string memory _name, string memory _ticker) ERC721(_name, _ticker) {
+        _owner = msg.sender;
+    }
+    
+    function mint(string memory _tokenURI, uint _price) public onlyOwner returns (bool) {
+        uint _tokenId = COUNTER;    
+        Ticket memory newTicket = Ticket(COUNTER, _price);
+        tickets.push(newTicket);
+        _mint(address(this), _tokenId);
+        _setTokenURI(_tokenId, _tokenURI);
+        COUNTER++;
+        return true;
+    }
+
+    function buyTicket(uint _id) public returns (bool) {
         
     }
+
+    function getTickets() public view returns (Ticket[] memory) {
+        return tickets;
+    }    
 }
