eae9219fd527c066fd8e8e4d71f21dd1217eed64
Avoaja Joshua Akachi <akachijc@gmail.com>
2022-02-22 21:08:35 +0000

    modified solidity compiler version

--- backend/contracts/CustomCollection.sol
+++ backend/contracts/CustomCollection.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.16 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
